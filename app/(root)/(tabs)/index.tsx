a4afc47c6e29d6252526dc412b8c4b443817eec0
Tal <tal.gutfreund@gmail.com>
2025-10-06 12:21:45 +0300

    fixed home page bug

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -67,7 +67,7 @@ export default function Index() {
       <FlatList
         data={properties}
         renderItem={({ item }) => <Card item={item} onPress={() => handleCardPress(item.$id)} />}
-        keyExtractor={(item) => item.toString()}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
@@ -104,7 +104,7 @@ export default function Index() {
                 <FlatList
                   data={latestProperties}
                   renderItem={({ item }) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
-                  keyExtractor={(item) => item.toString()}
+                  keyExtractor={(item) => item.$id}
                   horizontal
                   showsHorizontalScrollIndicator={false}
                   bounces={false}
--- components/Filters.tsx
+++ components/Filters.tsx
@@ -22,7 +22,7 @@ const Filters = () => {
     <ScrollView horizontal showsHorizontalScrollIndicator={false}
     className="mt-3 mb-2">
         {categories.map((item, index) => (
-            <TouchableOpacity onPress={() => handleCategroryPress(item.category)} 
+            <TouchableOpacity key={index} onPress={() => handleCategroryPress(item.category)} 
             className={`flex flex-col items-start mr-4 px-4 py-2 
             rounded-full ${selectedCategory === item.category ? 'bg-primary-300' : 
             'bg-primary-100 border border-primary-200'}`}>
